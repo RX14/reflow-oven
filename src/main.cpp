bddc8fcb48e6ac2252b6fa9a8adcb1386ecfc5a8
Stephanie Wilde-Hobbs <steph@rx14.co.uk>
2020-09-24 17:42:02 +0100

    Remove PID autotune

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <PID_AutoTune_v0.h>
 #include <PID_v1.h>
-#include <SPI.h>
 
 #define TIME_WINDOW 512
 #define PID_WINDOW TIME_WINDOW * 2
@@ -20,9 +18,6 @@ double onTimeMs;
 double rawOnTimeMs;
 PID pid(&currentTemp, &onTimeMs, &targetTemp, 13.74, 0.05, 0, P_ON_M, DIRECT);
 
-bool tune = false;
-PID_ATune pid_autotune(&currentTemp, &onTimeMs);
-
 void setup() {
   analogReference(EXTERNAL);
 
@@ -30,11 +25,6 @@ void setup() {
 
   pid.SetOutputLimits(0, TIME_WINDOW);
   pid.SetSampleTime(PID_WINDOW);
-
-  pid_autotune.SetNoiseBand(2);
-  pid_autotune.SetOutputStep(TIME_WINDOW / 2);
-  pid_autotune.SetControlType(0);
-  pid_autotune.SetLookbackSec(30);
 
   pinMode(SSR_PIN, OUTPUT);
 }
@@ -46,6 +36,7 @@ double getTemp() {
   prtCMACount = 0;
 
   double rPRT = PRT_RESISTOR * ((2 / (prtCMASum / 1024)) - 1);
+
   // if (abs(rawTemp - targetTemp) < 3) {
   //   rawTemp = targetTemp;
   // }
@@ -65,32 +56,7 @@ void loop() {
     lastTempReading = now;
   }
 
-  if (tune) {
-    if (pid_autotune.Runtime()) {
-      Serial.print("Kp: ");
-      Serial.println(pid_autotune.GetKp());
-      Serial.print("Ki: ");
-      Serial.println(pid_autotune.GetKi());
-      Serial.print("Kd: ");
-      Serial.println(pid_autotune.GetKd());
-
-      digitalWrite(SSR_PIN, LOW);
-      while (1)
-        ;
-    }
-  } else {
-    // if (currentTemp - targetTemp < -50) {
-    //   pid.SetMode(MANUAL);
-    //   onTimeMs = TIME_WINDOW;
-    // } else if (currentTemp - targetTemp > 20) {
-    //   pid.SetMode(MANUAL);
-    //   onTimeMs = 0;
-    // } else {
-    pid.SetMode(AUTOMATIC);
-    // }
-
-    pid.Compute();
-  }
+  pid.Compute();
 
   unsigned long windowTime = now % TIME_WINDOW;
   if (windowTime < onTimeMs) {
@@ -100,6 +66,8 @@ void loop() {
   }
 
   if (now - lastWrite > 500) {
+    lastWrite = now;
+
     Serial.println();
     Serial.print("R: ");
     Serial.println(prtCMASum);
@@ -115,9 +83,6 @@ void loop() {
     Serial.println(pid.GetKi());
     Serial.print("Kd: ");
     Serial.println(pid.GetKd());
-    if (pid.GetMode() == MANUAL)
-      Serial.println("MANUAL");
-    lastWrite = now;
   }
 
   if (Serial.available() > 0) {
@@ -137,12 +102,6 @@ void loop() {
     } else if (cmd == 's') {
       Serial.write("Set: ");
       targetTemp = Serial.parseFloat();
-    } else if (cmd == ' ') {
-      onTimeMs = TIME_WINDOW / 2;
-      tune = !tune;
     }
-
-    Serial.print("Tune: ");
-    Serial.println(tune);
   }
 }
